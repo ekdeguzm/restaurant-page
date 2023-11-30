b7449a2c914e7412582add032fce4a960b984881
Kawayan <kawayan.deguzman@gmail.com>
2023-11-29 19:09:52 -0800

    create menu and contact form

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+// module for creating the contact page
+
+const createContactPage = () => {
+  const content = document.querySelector('#content')
+  const pageContent = document.createElement('div')
+  pageContent.classList.add('page-content');
+
+  
+}

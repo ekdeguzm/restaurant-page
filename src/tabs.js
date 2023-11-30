abc279cc595a1ce0a71ab5c1e8792b6561c7b55c
Kawayan <kawayan.deguzman@gmail.com>
2023-11-29 19:21:28 -0800

    create ids, classes, text content, and append to content div

--- src/tabs.js
+++ src/tabs.js
@@ -4,7 +4,30 @@ const createTabs = () => {
   const content = document.querySelector('#content');
 
 
-  // 
+  // create the three divs
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  const div3 = document.createElement('div');
+
+  // set ids for the divs
+  div1.setAttribute('id', 'home-btn');
+  div2.setAttribute('id', 'menu-btn');
+  div3.setAttribute('id', 'contact-btn');
+
+  // set classes for the divs
+  div1.classList.add('tab');
+  div2.classList.add('tab');
+  div3.classList.add('tab');
+
+  // set text content for the tabs
+  div1.textContent = 'Home';
+  div2.textContent = 'Menu';
+  div3.textContent = 'Contact';
+
+  // append the divs to the content div
+  content.appendChild(div1)
+  content.appendChild(div2)
+  content.appendChild(div3)
 }
 
 export default createTabs;

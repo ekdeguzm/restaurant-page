bbcc4fec9c38345c9d172c20d3152344de109be3
Kawayan <kawayan.deguzman@gmail.com>
2023-11-30 20:42:20 -0800

    create contact page

--- src/contact.js
+++ src/contact.js
@@ -13,8 +13,23 @@ const createContactPage = () => {
   headingInput.placeholder = 'Enter heading';
   form.appendChild(headingInput);
 
+  const addressInput = document.createElement('input');
+  addressInput.type = 'text';
+  addressInput.placeholder = 'Enter address';
+  form.appendChild(addressInput);
 
+  const phoneInput = document.createElement('input');
+  phoneInput.type = 'text';
+  phoneInput.placeholder = 'Enter phone';
+  form.appendChild(phoneInput);
 
+  const submitButton = document.createElement('input');
+  submitButton.type = 'submit';
+  submitButton.placeholder = 'Submit'
+  form.appendChild(submitButton);
+
+  pageContent.appendChild(form);
+  content.appendChild(pageContent)
 }
 
 export default createContactPage;

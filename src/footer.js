58cc2442d50f0b4f9312c734148cd082788228db
Kawayan <kawayan.deguzman@gmail.com>
2023-12-17 15:07:36 -0800

    center footer

--- src/footer.js
+++ src/footer.js
@@ -4,19 +4,31 @@ function createFooter() {
   footer.classList.add("footer");
 
   const copyright = document.createElement("p");
-  copyright.textContent = `Copyright © ${new Date().getFullYear()} ekdeguzm`;
 
+  // Create a span element for the name "ekdeguzm"
+  const nameSpan = document.createElement("span");
+  nameSpan.textContent = "ekdeguzm";
+
+  // Create a link element for the GitHub profile
   const githubLink = document.createElement("a");
   githubLink.href = "https://github.com/ekdeguzm";
+  githubLink.target = "_blank"; // Open link in a new tab
 
-  const githubIcon = document.createElement("i");
-  githubIcon.classList.add("fab");
-  githubIcon.classList.add("fa-github");
+  // Use Unicode character for the GitHub icon
+  const githubIcon = document.createTextNode("\u{1F5A5}"); // Unicode for GitHub icon
 
   githubLink.appendChild(githubIcon);
+
+  // Append the name span and GitHub link to the copyright paragraph
+  copyright.appendChild(document.createTextNode("Copyright © "));
+  copyright.appendChild(nameSpan);
+  copyright.appendChild(document.createTextNode(" "));
+  copyright.appendChild(githubLink);
+
+  // Append the copyright paragraph to the footer
   footer.appendChild(copyright);
-  footer.appendChild(githubLink);
-  
+
+  // Append the footer to the content
   content.appendChild(footer);
 
   return footer;
